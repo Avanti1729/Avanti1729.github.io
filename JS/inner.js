9be7d87b0080771f6ad6d9fb8e2a6c2670c3a551
Avanti1729 <avantikagupta1729@gmail.com>
2025-12-17 22:17:13 +0530

    New Project made

--- JS/inner.js
+++
@@ -1,8 +0,0 @@
-const first = document.querySelector(".first");
-console.log(first.innerText);
-
-const second = document.querySelector(".second");
-console.log(second.textContent);
-
-const third = document.querySelector(".third");
-console.log(third.innerHTML);
